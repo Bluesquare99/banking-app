a27c9874ee5da64af4456787ddd65d97ceb18ef0
Max Mccready <maxmccready@Maxs-MacBook-Pro.local>
2026-03-10 13:14:11 -0700

    Added persistent volumes

--- app/main.py
+++ app/main.py
@@ -1,26 +1,39 @@
 from fastapi import FastAPI
+from pathlib import Path
 from pydantic import BaseModel
 from enum import Enum
 
 app = FastAPI()
 
+volume_path = Path("/ledger")
+stocks_file = str(volume_path / "stock.txt") if volume_path.exists() else "stock.txt"
+stocks_file.touch()
 
-class TransactionType(str, Enum):
-    deposit = "deposit"
-    withdrawal = "withdrawal"
+class OrderType(str, Enum):
+    buy = "buy"
+    sell = "sell"
 
-
-class Transaction(BaseModel):
-    user_id: int
-    transaction_type: TransactionType
+class Order(BaseModel):
+    order_type: OrderType
     amount: int
+
+def enter_order(order: Order):
+    with open(stocks_file, 'r') as f:
+        cur_price = int(f.read())
+    if order.order_type == OrderType.buy:
+        cur_price += order.amount
+    elif order.order_type == OrderType.sell:
+        cur_price -= order.amount
+    with open(stocks_file, 'w') as f:
+        f.write(str(cur_price))
 
 
 @app.get("/")
 async def root():
-    return "Welcome to Max's bank!"
+    return "Welcome to Max's exchange!"
 
 
-@app.post("/transaction/{user_id}")
-async def transaction(user_id: int, transaction: Transaction):
-    return transaction
+@app.post("/order/")
+async def order(order: Order):
+    enter_order(order)
+    return order
